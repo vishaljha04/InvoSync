4f4639ec84dce25a1bd5fbdee48fd69a5b8e1c06
Vishal Jha <vjha93011@gmail.com>
2025-12-09 16:48:33 +0530

    feat: enhance CreateInvoice component with AI data integration and invoice number generation

--- src/pages/Invoices/CreateInvoice.jsx
+++ src/pages/Invoices/CreateInvoice.jsx
@@ -1,19 +1,144 @@
-import { useState,useEffect } from "react"
-import { useNavigate,useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 import axiosInstance from "../../utils/axiosInstance";
 import { API_PATHS } from "../../utils/apiPath";
-import { Plus,Trash2 } from "lucide-react";
+import { Plus, Trash2 } from "lucide-react";
 import toast from "react-hot-toast";
 import moment from "moment";
-import {useAuth} from '../../context/AuthContext';
+import { useAuth } from "../../context/AuthContext";
 
+const CreateInvoice = ({ existingInvoice, onSave }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { user } = useAuth();
+  const [FormData, setFormData] = useState(
+    existingInvoice || {
+      invoiceNumber: "",
+      invoiceDate: new Date().toISOString().split("T")[0],
+      dueDate: "",
+      billFrom: {
+        businessName: user?.businessName || "",
+        email: user?.email || "",
+        address: user?.address || "",
+        phone: user?.phone || "",
+      },
+      billTo: {
+        clientName: "",
+        email: "",
+        address: "",
+        phone: "",
+      },
+      items: [
+        {
+          name: "",
+          quantity: 1,
+          unitPrice: 0,
+          taxPercent: 0,
+        },
+      ],
+      notes: "",
+      paymentTerms: "Net 15",
+    }
+  );
 
-const CreateInvoice = () => {
+  const [laoding, setLoading] = useState(false);
+  const [isGeneratingNumber, setisGeneratingNumber] = useState(
+    !existingInvoice
+  );
+
+  useEffect(() => {
+    const aiData = location.state?.aiData;
+
+    if (aiData) {
+      setFormData((prev) => ({
+        ...prev,
+        billTo: {
+          clientName: aiData.clientName || "",
+          email: aiData.email || "",
+          address: aiData.email || "",
+          phone: "",
+        },
+        items: aiData.items || {
+          name: "",
+          quantity: 1,
+          unitPrice: 0,
+          taxPercent: 0,
+        },
+      }));
+    }
+
+    if (existingInvoice) {
+      setFormData({
+        ...existingInvoice,
+        invoiceDate: moment(existingInvoice.invoiceDate).format("YYYY-MM-DD"),
+        dueDate: moment(existingInvoice.dueDate).format("YYYY-MM-DD"),
+      });
+    } else {
+      const generateNewInvoiceNumber = async () => {
+        setisGeneratingNumber(true);
+        try {
+          const response = await axiosInstance.get(
+            API_PATHS.INVOICE.GET_ALL_INVOICES
+          );
+          const invoices = response.data;
+          let maxNum = 0;
+          invoices.forEach((inv) => {
+            const num = parseInt(inv.invoiceNumber.split("-")[1]);
+            if (isNaN(num) && num > maxNum) {
+              maxNum = num;
+            }
+          });
+          const NewInvoiceNumber = `INV-${String(maxNum + 1).padStart(3, "0")}`;
+          setFormData((prev) => ({ ...prev, invoiceNumber: NewInvoiceNumber }));
+        } catch (error) {
+          console.error("Failed to Generate invoice number", error);
+          setFormData((prev) => ({
+            ...prev,
+            invoiceNumber: `INV-${Date.now().toString().slice(-5)}`,
+          }));
+        }
+        setisGeneratingNumber(false);
+      };
+
+      generateNewInvoiceNumber();
+    }
+  }, [existingInvoice]);
+
+  const handleInputChange = (e, section, index) => {};
+
+  const handleAddItem = () => {
+    setFormData({
+      ...FormData,
+      items: [
+        ...FormData.items,
+        { name: "", quantity: 1, unitPrice: 0, taxPercent: 0 },
+      ],
+    });
+  };
+
+  const handleRemoveItem = (index) => {};
+
+  const { subTotal, taxTotal, total } = (() => {
+    let subTotal = 0,
+      taxTotal = 0;
+    FormData.items.forEach((item) => {
+      const itemTotal = (item.quantity || 0) * (item.unitPrice || 0);
+      subTotal += itemTotal;
+      taxTotal += itemTotal * ((item.taxPercent || 0) / 100);
+    });
+    return { subTotal, taxTotal, total: subTotal + taxTotal };
+  })();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+  };
+
   return (
     <div>
-      
+      <div></div>
     </div>
-  )
-}
+  );
+};
 
-export default CreateInvoice
+export default CreateInvoice;
