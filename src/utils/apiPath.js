fa7d4732e4a5d75d8704ec34f6ef68f2aead9f2f
Vishal Jha <vjha93011@gmail.com>
2025-12-17 16:42:09 +0530

    profile page commented

--- src/utils/apiPath.js
+++ src/utils/apiPath.js
@@ -1,22 +1,22 @@
-export const BASE_URL = "http://192.168.29.95:5000";
+export const BASE_URL = "https://invosyncbackend.vercel.app/";
 
 export const API_PATHS = {
   AUTH: {
-    REGISTER: "/api/auth/register",
-    LOGIN: "/api/auth/login",
-    GET_PROFILE: "/api/auth/me",
-    UPDATE_PROFILE: "/api/auth/me",
+    REGISTER: "api/auth/register",
+    LOGIN: "api/auth/login",
+    GET_PROFILE: "api/auth/me",
+    UPDATE_PROFILE: "api/auth/me",
   },
   INVOICE: {
-    CREATE: "/api/invoices/",
-    GET_ALL_INVOICES: "/api/invoices/",
-    GET_ALL_INVOICE_BY_ID: (id) => `/api/invoices/${id}`,
-    UPDATE_INVOICE: (id) => `/api/invoices/${id}`,
-    DELETE_INVOICE: (id) => `/api/invoices/${id}`,
+    CREATE: "api/invoices/",
+    GET_ALL_INVOICES: "api/invoices/",
+    GET_ALL_INVOICE_BY_ID: (id) => `api/invoices/${id}`,
+    UPDATE_INVOICE: (id) => `api/invoices/${id}`,
+    DELETE_INVOICE: (id) => `api/invoices/${id}`,
   },
   AI: {
-    PARSE_INVOICE_TEXT: "/api/ai/parse-text",
-    GENERATE_REMAINDER: "/api/ai/generate-remainder",
-    GET_DASHBOARD_SUMMARY: "/api/ai/dashboard-summary",
+    PARSE_INVOICE_TEXT: "api/ai/parse-text",
+    GENERATE_REMAINDER: "api/ai/generate-remainder",
+    GET_DASHBOARD_SUMMARY: "api/ai/dashboard-summary",
   },
 };
